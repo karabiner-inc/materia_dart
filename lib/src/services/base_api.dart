5d7afbf476af057d018f54df39e999140a89859a
karabiner-kawakami <lee.kawakami@karabiner.tech>
2019-01-31 18:02:48 +0900

    add class #19

--- lib/src/services/base_api.dart
+++ lib/src/services/base_api.dart
@@ -2,35 +2,39 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-Future<http.Response> get(String path, {String token = ''}) async {
-  return http.get(path, headers: {
-    HttpHeaders.authorizationHeader: 'Bearer $token',
-    HttpHeaders.acceptHeader: 'application/json',
-    HttpHeaders.contentTypeHeader: 'application/json',
-  });
-}
+class BaseAPI {
+  
+  Future<http.Response> get(String path, {String token = ''}) async {
+    return http.get(path, headers: {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.contentTypeHeader: 'application/json',
+    });
+  }
 
-Future<http.Response> post(String path, Map<String, dynamic> data, {String token = ''}) async {
-  return http
-      .post(path, body: json.encode(data), headers: {
-    HttpHeaders.authorizationHeader: 'Bearer $token',
-    HttpHeaders.acceptHeader: 'application/json',
-    HttpHeaders.contentTypeHeader: 'application/json',
-  });
-}
+  Future<http.Response> post(String path, Map<String, dynamic> data,
+      {String token = ''}) async {
+    return http.post(path, body: json.encode(data), headers: {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.contentTypeHeader: 'application/json',
+    });
+  }
 
-Future<http.Response> put(String path, Map<String, dynamic> data, {String token = ''}) async {
-  return http.put(path, body: json.encode(data), headers: {
-    HttpHeaders.authorizationHeader: 'Bearer $token',
-    HttpHeaders.acceptHeader: 'application/json',
-    HttpHeaders.contentTypeHeader: 'application/json',
-  });
-}
+  Future<http.Response> put(String path, Map<String, dynamic> data,
+      {String token = ''}) async {
+    return http.put(path, body: json.encode(data), headers: {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.contentTypeHeader: 'application/json',
+    });
+  }
 
-Future<http.Response> delete(String path, {String token = ''}) async {
-  return http.delete(path, headers: {
-    HttpHeaders.authorizationHeader: 'Bearer $token',
-    HttpHeaders.acceptHeader: 'application/json',
-    HttpHeaders.contentTypeHeader: 'application/json',
-  });
+  Future<http.Response> delete(String path, {String token = ''}) async {
+    return http.delete(path, headers: {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      HttpHeaders.acceptHeader: 'application/json',
+      HttpHeaders.contentTypeHeader: 'application/json',
+    });
+  }
 }
