e32381f07a11cd944aba8ee8460a54692bc8f4b4
karabiner-kawakami <lee.kawakami@karabiner.tech>
2019-02-12 11:34:14 +0900

    add materia models

--- lib/materia_career_dart.dart
+++ lib/materia_career_dart.dart
@@ -1,4 +1,12 @@
 // models
+export 'src/models/materia/account.dart';
+export 'src/models/materia/address.dart';
+export 'src/models/materia/grant.dart';
+export 'src/models/materia/mail_template.dart';
+export 'src/models/materia/organization.dart';
+export 'src/models/materia/response_auth.dart';
+export 'src/models/materia/token.dart';
+export 'src/models/materia/user.dart';
 export 'src/models/materia_career/offer.dart';
 export 'src/models/materia_career/project.dart';
 export 'src/models/materia_career/project_tag.dart';
