25919c21c42f47686e4f78e6ddb0e2e6e75ec088
karabiner-kawakami <lee.kawakami@karabiner.tech>
2019-02-13 11:04:22 +0900

    add score column to records model #26

--- test/materia_career_test.dart
+++ test/materia_career_test.dart
@@ -2,17 +2,16 @@ import 'package:test/test.dart';
 import 'package:materia_dart/materia_career_dart.dart';
 import 'package:materia_dart/materia_dart.dart';
 
-const String basePath='http://localhost:4001/api';
-const String basePathOps='http://localhost:4001/api/ops';
+const String basePath = 'http://localhost:4001/api';
+const String basePathOps = 'http://localhost:4001/api/ops';
 
 void main() {
-
   Token accessToken;
   MateriaCareerAPI api = MateriaCareerAPI();
 
   String _timestamp() => (DateTime.now().millisecondsSinceEpoch + 1).toString();
 
-  setUpAll(() async{
+  setUpAll(() async {
     var data = {
       'email': 'hogehoge@example.com',
       'password': 'hogehoge',
@@ -26,54 +25,62 @@ void main() {
   });
 
   test('list my projects', () async {
-    var data = {'status_list': [1,2]};
-    final List<Project> projects = await api.listMyProjects(basePath, data, accessToken.accessToken);
+    var data = {
+      'status_list': [1, 2]
+    };
+    final List<Project> projects =
+        await api.listMyProjects(basePath, data, accessToken.accessToken);
     expect(projects.isNotEmpty, true);
   });
 
   test('create and update my projects', () async {
     var data = {'title': 'project1'};
-    final Project projects = await api.createMyProject(basePath, data, accessToken.accessToken);
+    final Project projects =
+        await api.createMyProject(basePath, data, accessToken.accessToken);
     expect(projects.title, 'project1');
-    var updateData = {
-      'id': projects.id,
-      'title': 'project2'
-    };
-    final Project updatedProjects = await api.updateMyProject(basePath, updateData, accessToken.accessToken);
+    var updateData = {'id': projects.id, 'title': 'project2'};
+    final Project updatedProjects = await api.updateMyProject(
+        basePath, updateData, accessToken.accessToken);
     expect(updatedProjects.title, 'project2');
   });
 
   test('list my offers', () async {
     var data = {'status': '1'};
-    final List<Offer> offers = await api.listMyOffers(basePath, data, accessToken.accessToken);
+    final List<Offer> offers =
+        await api.listMyOffers(basePath, data, accessToken.accessToken);
     expect(offers.isNotEmpty, true);
   });
 
   test('list-my-projects-offers', () async {
-    var data = {'status': '1', 'project_id_list': [1]};
-    final List<Offer> offers = await api.listMyProjectsOffers(basePath, data, accessToken.accessToken);
+    var data = {
+      'status': '1',
+      'project_id_list': [1]
+    };
+    final List<Offer> offers =
+        await api.listMyProjectsOffers(basePath, data, accessToken.accessToken);
     expect(offers.isEmpty, true);
   });
 
   test('create-my-organization-offer', () async {
-
     var data = {'title': 'project1', 'status': 2};
-    final Project projects = await api.createMyProject(basePath, data, accessToken.accessToken);
+    final Project projects =
+        await api.createMyProject(basePath, data, accessToken.accessToken);
 
     var createOfferData = {
-    'project_id': projects.id,
-    'offer_message': 'test offer',
-    'message_subject': 'test',
-    'status': 1
+      'project_id': projects.id,
+      'offer_message': 'test offer',
+      'message_subject': 'test',
+      'status': 1
     };
-    final Offer offers = await api.createMyOrganizationOffer(basePath, createOfferData, accessToken.accessToken);
+    final Offer offers = await api.createMyOrganizationOffer(
+        basePath, createOfferData, accessToken.accessToken);
     expect(offers.offerMessage, 'test offer');
   });
 
   test('create-my-offer', () async {
-
     var data = {'title': 'project1', 'status': 2};
-    final Project projects = await api.createMyProject(basePath, data, accessToken.accessToken);
+    final Project projects =
+        await api.createMyProject(basePath, data, accessToken.accessToken);
 
     var createOfferData = {
       'project_id': projects.id,
@@ -81,14 +88,15 @@ void main() {
       'message_subject': 'test',
       'status': 1
     };
-    final Offer offers = await api.createMyOffer(basePath, createOfferData, accessToken.accessToken);
+    final Offer offers = await api.createMyOffer(
+        basePath, createOfferData, accessToken.accessToken);
     expect(offers.offerMessage, 'test offer');
   });
 
   test('update-my-organization-offer', () async {
-
     var data = {'title': 'project1', 'status': 2};
-    final Project projects = await api.createMyProject(basePath, data, accessToken.accessToken);
+    final Project projects =
+        await api.createMyProject(basePath, data, accessToken.accessToken);
 
     var createOfferData = {
       'project_id': projects.id,
@@ -96,7 +104,8 @@ void main() {
       'message_subject': 'test',
       'status': 1
     };
-    final Offer offers = await api.createMyOrganizationOffer(basePath, createOfferData, accessToken.accessToken);
+    final Offer offers = await api.createMyOrganizationOffer(
+        basePath, createOfferData, accessToken.accessToken);
 
     var updateOfferData = {
       'offer_id': offers.id,
@@ -106,15 +115,15 @@ void main() {
       'status': 2
     };
 
-    final Offer updatedOffer = await api.updateMyOrganizationOffer(basePath, updateOfferData, accessToken.accessToken);
+    final Offer updatedOffer = await api.updateMyOrganizationOffer(
+        basePath, updateOfferData, accessToken.accessToken);
     expect(updatedOffer.offerMessage, 'updated test offer');
-
   });
 
   test('update-my-offer', () async {
-
     var data = {'title': 'project1', 'status': 2};
-    final Project projects = await api.createMyProject(basePath, data, accessToken.accessToken);
+    final Project projects =
+        await api.createMyProject(basePath, data, accessToken.accessToken);
 
     var createOfferData = {
       'project_id': projects.id,
@@ -122,7 +131,8 @@ void main() {
       'message_subject': 'test',
       'status': 1
     };
-    final Offer offers = await api.createMyOffer(basePath, createOfferData, accessToken.accessToken);
+    final Offer offers = await api.createMyOffer(
+        basePath, createOfferData, accessToken.accessToken);
 
     var updateOfferData = {
       'offer_id': offers.id,
@@ -132,15 +142,15 @@ void main() {
       'status': 2
     };
 
-    final Offer updatedOffer = await api.updateMyOffer(basePath, updateOfferData, accessToken.accessToken);
+    final Offer updatedOffer = await api.updateMyOffer(
+        basePath, updateOfferData, accessToken.accessToken);
     expect(updatedOffer.offerMessage, 'updated test offer');
-
   });
 
   test('update-my-organization-offer', () async {
-
     var data = {'title': 'project1', 'status': 2};
-    final Project projects = await api.createMyProject(basePath, data, accessToken.accessToken);
+    final Project projects =
+        await api.createMyProject(basePath, data, accessToken.accessToken);
 
     var createOfferData = {
       'project_id': projects.id,
@@ -148,7 +158,8 @@ void main() {
       'message_subject': 'test',
       'status': 1
     };
-    final Offer offers = await api.createMyOrganizationOffer(basePath, createOfferData, accessToken.accessToken);
+    final Offer offers = await api.createMyOrganizationOffer(
+        basePath, createOfferData, accessToken.accessToken);
 
     var updateOfferData = {
       'offer_id': offers.id,
@@ -157,11 +168,10 @@ void main() {
       'lock_version': offers.lockVersion,
     };
 
-    final Offer updatedOffer = await api.answerOfferToMyOrganization(basePath, updateOfferData, accessToken.accessToken);
+    final Offer updatedOffer = await api.answerOfferToMyOrganization(
+        basePath, updateOfferData, accessToken.accessToken);
     expect(updatedOffer.answerMessage, 'answer message');
-
   });
-
 
 //  test('answer-offer-to-me', () async {
 //    var data = {'status': '1'};
@@ -182,38 +192,44 @@ void main() {
 //  records
 
   test('list-my-records', () async {
-    final List<Record> records = await api.listMyRecords(basePath, accessToken.accessToken);
+    final List<Record> records =
+        await api.listMyRecords(basePath, accessToken.accessToken);
     expect(records.isNotEmpty, true);
-
   });
 
   test('create-my-record', () async {
     var data = {
       'title': 'title1',
-      'discription': 'discription1',
+      'description': 'description1',
+      'score': 10.0,
       'project_id': 1
     };
-    final Record record = await api.createMyRecord(basePath, data, accessToken.accessToken);
+    final Record record =
+        await api.createMyRecord(basePath, data, accessToken.accessToken);
     expect(record.title, 'title1');
-
+    expect(record.score, 10.0);
   });
 
   test('update-my-record', () async {
     var data = {
       'title': 'title1',
-      'discription': 'discription1',
+      'description': 'description1',
+      'score': 10.0,
       'project_id': 1
     };
-    final Record record = await api.createMyRecord(basePath, data, accessToken.accessToken);
+    final Record record =
+        await api.createMyRecord(basePath, data, accessToken.accessToken);
 
     var updateData = {
       'id': record.id,
       'title': 'updated title1',
-      'discription': 'updated discription1',
+      'description': 'updated description1',
+      'score': 12.0,
       'project_id': 1
     };
-    final Record updatedRecord = await api.updateMyRecord(basePath, updateData, accessToken.accessToken);
+    final Record updatedRecord =
+        await api.updateMyRecord(basePath, updateData, accessToken.accessToken);
     expect(updatedRecord.title, 'updated title1');
+    expect(updatedRecord.score, 12.0);
   });
-
 }
